3cc6751ff96a4aec51f2a6984309c017d289f7e1
judeadesuluflyer6@gmail.com <jude.adesulu@flutterwavego.com>
2023-02-03 13:10:45 +0100

    The domain

---
+++ HR.LeaveManagement.Clean.Domain/LeaveType.cs
@@ -0,0 +1,18 @@
+﻿using HR.LeaveManagement.Clean.Domain.common;
+
+namespace HR.LeaveManagement.Clean.Domain
+{
+    public class LeaveType : BaseDomainEntity
+    {
+        /*public LeaveType(string name, int defaultDays)
+        {
+            Name = name;
+            DefaultDays = defaultDays;
+        }*/
+
+        public string Name { get; set; }
+        public int DefaultDays { get; set; }
+        public DateTime DateCreated { get; set; }
+
+    }
+}
